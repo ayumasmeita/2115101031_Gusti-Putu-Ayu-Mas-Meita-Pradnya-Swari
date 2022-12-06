f010f84a7cb66675d36e73f5bd61c07103ff31c1
Gusti Putu Ayu Mas Meita Pradnya Swari <113962012+ayumasmeita@users.noreply.github.com>
2022-12-06 12:47:17 +0800

    Add files via upload

---
+++ SIAK 21A/id/ac/undiksha/siak/Main.java
@@ -0,0 +1,39 @@
+package id.ac.undiksha.siak;
+
+import id.ac.undiksha.siak.entitas.Dosen;
+import id.ac.undiksha.siak.entitas.Mahasiswa;
+import id.ac.undiksha.siak.entitas.Pegawai;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Mahasiswa mhs1 = new Mahasiswa();
+		mhs1.setNama("gekmas");
+		
+		Dosen dsn1 = new Dosen();
+		dsn1.setAlamat("Negare");
+		
+		Pegawai pgw1 = new Pegawai();
+		pgw1.setNip("2115101031");
+		
+		
+		System.out.println(mhs1.getNama());
+		System.out.println(dsn1.getAlamat());
+		System.out.println(pgw1.getNip());
+		mhs1.printAllInfo();
+		
+		
+		Mahasiswa mhs2 = new Mahasiswa (
+				"Budi",
+				"Singaraja",
+				"2115101031",
+				"ilmu komputer",
+				"teknik informatika",
+				"ftk"
+				
+				);
+				
+	}
+	
+}
